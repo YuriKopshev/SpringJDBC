d9b800e48f7a9abff86fbcbeae01efc5220ea608
“Yuri <yura.kopshev@gmail.com>
2021-10-30 15:29:08 +0400

    fixed #2

---
+++ src/main/resources/db/changelog/migrations/schema_001.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TESTERS
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    experience INT NOT NULL,
+    PRIMARY KEY (id)
+);
